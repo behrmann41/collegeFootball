d0842a11f729f57e75c8d451e71812f9bc98b64c
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-27 13:28:30 -0600

    CRUD - create complete

--- routes/teams.js
+++ routes/teams.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/college-teams');
+var collegeTeams = db.get('teams');
 
-router.get('/', function (req, res, next){
-  res.render('teams/index', { title: "College Teams"})
+router.get('/', function (req, res, next) {
+  collegeTeams.find({}, function (err, data){
+    res.render('teams/index', { title: "College Teams",
+                                allTeams: data
+                              })
+  })
 })
 
 router.get('/new', function (req, res, next){
   res.render('teams/newteam', { title: "Enter a team"})
+})
+
+router.post('/', function (req, res, next) {
+  collegeTeams.insert({ school: req.body.collegename,
+                        location: req.body.city,
+                        mascot: req.body.mascot,
+                        accredited: req.body.accredited
+                      });
+  res.redirect('/teams');
 })
 
 
